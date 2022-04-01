63dc5863128f429a01ff80b3dfcd660ca5cb9326
Federico Muntuny <FMUNTUNY@GMAIL.COM>
2022-03-31 22:10:14 -0300

    Modificacion seccion envios

--- js/jsCarrito.js
+++ js/jsCarrito.js
@@ -1,15 +1,18 @@
 const items = document.getElementById('items')
 const footer = document.getElementById('footer')
+const envios = document.getElementById('footer-envio')
 const templateCarrito = document.getElementById('template-carrito').content
 const templateFooter = document.getElementById('template-footer').content
+const templateEnvio = document.getElementById('template-envio').content
 const fragment = document.createDocumentFragment()
-
 let carrito = {}
+let user = {}
 
 //DETECTA QUE EL DOM ESTA CARGADO PARA PODER CARGAR LOS PRODUCTOS
 document.addEventListener('DOMContentLoaded', ()=>{
-    if (localStorage.getItem("usuario")) { 
-        carrito = JSON.parse(localStorage.getItem("usuario"));
+    if (localStorage.getItem("carrito")) { 
+        carrito = JSON.parse(localStorage.getItem("carrito"));
+        user = JSON.parse(localStorage.getItem("usuario"));
         pintarCarrito()
     }
     
@@ -25,8 +28,9 @@ const pintarCarrito = () => {
 items.innerHTML=''
 Object.values(carrito).forEach(prod =>{
     templateCarrito.querySelector('th').textContent = prod.id
-    templateCarrito.querySelectorAll('td')[0].textContent=prod.title
-    templateCarrito.querySelectorAll('td')[1].textContent=prod.cantidad
+    templateCarrito.querySelectorAll('td')[0].textContent = prod.title
+    templateCarrito.querySelectorAll('td')[1].textContent=prod.precio
+    templateCarrito.querySelectorAll('td')[2].textContent=prod.cantidad
     templateCarrito.querySelector('.btn-info').dataset.id=prod.id
     templateCarrito.querySelector('.btn-danger').dataset.id=prod.id
     templateCarrito.querySelector('span').textContent=prod.cantidad*prod.precio
@@ -34,19 +38,20 @@ Object.values(carrito).forEach(prod =>{
     fragment.appendChild(clone)
 })
 items.appendChild(fragment)
-pintarFooter()
-localStorage.setItem("usuario", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+    pintarFooter()
+localStorage.setItem("carrito", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
 }
 
 const pintarFooter = () => {
     if (Object.keys(carrito).length === 0) {
         footer.innerHTML = `<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>`
+        envios.innerHTML=''
         return
     } 
     footer.innerHTML=''
         const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
         const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0)
-        templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+        templateFooter.querySelectorAll('td')[1].textContent = nCantidad
         templateFooter.querySelector('span').textContent = nPrecio
         const clone = templateFooter.cloneNode(true)
         fragment.appendChild(clone)
@@ -56,6 +61,7 @@ const pintarFooter = () => {
             carrito = {}
             pintarCarrito()
         })
+        pintarEnvio()
 }
 const btnAccion = e => { 
     if (e.target.classList.contains('btn-info')) { 
@@ -75,3 +81,16 @@ const btnAccion = e => {
     e.stopPropagation()
 }
 
+//FALTA HACER EL PINTAENVIO!!!!!!!!!!!!!!!!!!!!!!!!!!!!1*********************************************
+const pintarEnvio = () => { 
+    envios.innerHTML=''
+    templateEnvio.getElementById('nombre').textContent = user.nombre
+    templateEnvio.getElementById('apellido').textContent = user.apellido
+    templateEnvio.getElementById('direccion').textContent = user.direccion
+    templateEnvio.getElementById('telefono').textContent = user.telefono
+    templateEnvio.getElementById('costo').textContent = 600
+    templateEnvio.getElementById('total-costo').textContent = Number(document.getElementById('total-compra').textContent) + 600
+    const clone = templateEnvio.cloneNode(true)
+    fragment.appendChild(clone)
+    envios.appendChild(fragment)
+}
--- js/jsIndex.js
+++ js/jsIndex.js
@@ -3,20 +3,20 @@ const irDetalleCarrito = document.getElementById('irDetalleCarrito')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
 let carrito = {}//AL CARGAR LA PÁGINA ESTE ARRAY SE TIENE QUE CARGAR CON TODOS LOS PRODUCTOS DE LA INDEXEDDB DEL CLIENTE
+let user = {}
 const activaBD = document.getElementById('irDetalleCarrito')
-
-
-
 
 
 //DETECTA EL CLICK EN EL A QUE TE LLEVA AL DETALLE DEL CARRITO Y AHÍ SE GUARDA EL CARRITO EN LA INDEXEDDB
 activaBD.addEventListener('click', () => {
-    localStorage.setItem("usuario", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+    localStorage.setItem("carrito", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
 })
 //DETECTA QUE EL DOM ESTA CARGADO PARA PODER CARGAR LOS PRODUCTOS
+//EL "USUARIO" DEL LOCALSTORAGE DEBERÍA SER EL ID DEL USER PARA PODER TENER DIFERENTES CARRITOS DE USERS ALMACENADO
 document.addEventListener('DOMContentLoaded', ()=>{
     if (localStorage.getItem("usuario")) { 
-        carrito = JSON.parse(localStorage.getItem("usuario"));
+        carrito = JSON.parse(localStorage.getItem("carrito"));
+        user = JSON.parse(localStorage.getItem("user"));
     }
     fetchdata()
 })
@@ -27,44 +27,98 @@ cards.addEventListener('click', e =>{
 const fetchdata = async () => {
     //CARGO LOS PRODUCTOS EN LA PÁGINA DE PRODUCTOS DESDE LA API
     try {
-        const res = await fetch('js/api.json')
-        const data = await res.json()
-        pintarCards(data)
+        const resPro = await fetch('js/apiProductos.json')
+        const dataPro = await resPro.json()
+        pintarCards(dataPro)
+        const resUser = await fetch('js/apiUser.json')
+        const dataUser = await resUser.json()
+        dataUser.forEach(u => { 
+            if (u.id===1) {//*********************SERÍA IF(IDQUEVIENEDELLOGIN === U.ID)**********************************
+                user = {
+                    id: u.id,
+                    nombre: u.nombre,
+                    apellido: u.apellido,
+                    direccion: u.direccion,
+                    telefono: u.telefono,
+                    mail: u.mail,
+                    fechaAlta: u.fechaAlta
+                }
+            }
+            localStorage.setItem("usuario", JSON.stringify(user));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+        })
     } catch (error) {
         console.log(error)
     }
 }
 
 //DIBUJA LOS PRODUCTOS EN LA PÁGINA
-const pintarCards = (data)=>{
-    data.forEach(producto =>{
-        templateCard.querySelector('h5').textContent=producto.title
-        templateCard.querySelector('p').textContent=producto.precio
+const pintarCards = (data) => {
+    data.forEach(producto => {
+        templateCard.getElementById('titulo').textContent = producto.title
+        templateCard.getElementById('descripcion').textContent = producto.descripcion
+        templateCard.getElementById('precio').textContent = "Precio: $ " + producto.precio
+        templateCard.getElementById('calificacion').textContent = "Calificacion: " + producto.calificacion      
         templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
-        templateCard.querySelector('.btn-dark').dataset.id = producto.id
+        templateCard.querySelector('.btn-info').dataset.id = producto.id
+        templateCard.querySelector('.btn-danger').dataset.id = producto.id
+        if (Object.values(carrito).length > 0) {
+            Object.values(carrito).some(prodCarrito => {
+                if (Number(prodCarrito.id) === producto.id) {
+                    templateCard.getElementById('cantidad').textContent = "Cantidad: " + prodCarrito.cantidad
+                    return true
+                } else {
+                    templateCard.getElementById('cantidad').textContent = "Cantidad: 0"
+                }
+            })
+        } else { 
+            templateCard.getElementById('cantidad').textContent = "Cantidad: 0"
+        }
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     })
     cards.appendChild(fragment)
 }
 //AGREGA EL PRODUCTO EN EL CARRITO UNA VEZ CLICKEADO EL BOTON DEL PRODUCTO
-const addCarrito = e =>{
-    if(e.target.classList.contains('btn-dark')){
-        setCarrito(e.target.parentElement)
+const addCarrito = (e) => {
+    //if(e.target.classList.contains('btn-dark')){
+    //    setCarrito(e.target.parentElement)
+    //}
+    if (e.target.classList.contains('btn-info')) {
+        setCarrito(e.target.parentElement, 1)
+    } else if (e.target.classList.contains('btn-danger')) { 
+        setCarrito(e.target.parentElement, -1)
     }
     e.stopPropagation()
 }
 //METE LOS DATOS DEL PRODUCTO EN EL ARRAY DE PRODUCTOS CARRITO
-const setCarrito = objeto =>{
+const setCarrito = (objeto, cant) => {
+    const largoPrecio = objeto.querySelector('#precio').textContent.length
+    const largoCantidad = objeto.querySelector('#cantidad').textContent.length
     const producto = {
-        id: objeto.querySelector('.btn-dark').dataset.id,
-        title: objeto.querySelector('h5').textContent,
-        precio: objeto.querySelector('p').textContent,
-        cantidad: 1
+        id: objeto.querySelector('.btn-info').dataset.id,
+        title: objeto.querySelector('#titulo').textContent,
+        precio: objeto.querySelector('#precio').textContent.substring(10,largoPrecio),
+        desccripcion: objeto.querySelector('#descripcion').textContent,
+        calificacion: objeto.querySelector('#calificacion').textContent,
+        cantidad: Number(objeto.querySelector('#cantidad').textContent.substring(10, largoCantidad))
     }
-    if(carrito.hasOwnProperty(producto.id)){
-        producto.cantidad=carrito[producto.id].cantidad + 1
-    }
-    carrito[producto.id] = { ...producto }
-    localStorage.setItem("usuario", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+    if (carrito.hasOwnProperty(producto.id)) {
+        if ((producto.cantidad + cant) > 0) {
+            producto.cantidad = carrito[producto.id].cantidad + cant
+            carrito[producto.id] = { ...producto }
+            localStorage.setItem("carrito", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+            objeto.querySelector('#cantidad').textContent= "Cantidad: " + producto.cantidad
+        } else if ((producto.cantidad + cant) < 1) {
+            objeto.querySelector('#cantidad').textContent = "Cantidad: 0"
+            delete carrito[producto.id]
+            localStorage.setItem("carrito", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+        }  
+    } else {
+        if (cant === 1) {
+            producto.cantidad++
+            carrito[producto.id] = { ...producto }
+            localStorage.setItem("carrito", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+            objeto.querySelector('#cantidad').textContent= "Cantidad: " + producto.cantidad
+        }
+    } 
 }
