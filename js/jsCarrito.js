e32671b5987f9f958a36b01a459c2094c1ae4629
Federico Muntuny <FMUNTUNY@GMAIL.COM>
2022-04-02 16:34:28 -0300

    Agregado selección tipo de envio y agrega costo envio al carrito del nav

--- js/jsCarrito.js
+++ js/jsCarrito.js
@@ -4,26 +4,33 @@ const envios = document.getElementById('footer-envio')
 const templateCarrito = document.getElementById('template-carrito').content
 const templateFooter = document.getElementById('template-footer').content
 const templateEnvio = document.getElementById('template-envio').content
+const templateOpcionEnvio = document.getElementById('template-opcion-envio')
 const fragment = document.createDocumentFragment()
+const rbEnvioLocal = document.getElementById('tipoEnvioLocal')
+const rbEnvioDomicilio=document.getElementById('tipoEnvioDomicilio')
+let envio=false
 let totalCompra=""
 let carrito = {}
 let user = {}
 
 //DETECTA QUE EL DOM ESTA CARGADO PARA PODER CARGAR LOS PRODUCTOS
-document.addEventListener('DOMContentLoaded', ()=>{
-    if (localStorage.getItem("carrito")) { 
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem("carrito")) {
         carrito = JSON.parse(localStorage.getItem("carrito"));
     }
-    if (localStorage.getItem("usuario")) { 
+    if (localStorage.getItem("usuario")) {
         user = JSON.parse(localStorage.getItem("usuario"));
     }
     if (localStorage.getItem("totalCompra")) {
         totalCompra = localStorage.getItem("totalCompra")
         document.getElementById('totalCompraNav').innerHTML = `$ ${totalCompra}`
-    } else { 
+    } else {
         localStorage.setItem("totalCompra", 0)
         totalCompra = 0
         document.getElementById('totalCompraNav').innerHTML = `$ ${totalCompra}`
+    }
+    if (!localStorage.getItem("envio")) { 
+        localStorage.setItem("envio", "no")
     }
     pintarCarrito()
 })
@@ -32,6 +39,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
 items.addEventListener('click', e => { 
     btnAccion(e)
 })
+
+//DETECTA EL CLICK EN LOS RADIOBUTTON
+document.getElementById('tipoEnvioLocal').addEventListener('click', () => { 
+    localStorage.setItem("envio", "no")
+    totalCompra = Number(totalCompra) - 600//SI SACA EL ENVIO, RESTAMOS EL ENVIO AL CARRITO DEL NAV
+    localStorage.setItem("totalCompra",totalCompra)
+    pintarCarrito()
+})
+document.getElementById('tipoEnvioDomicilio').addEventListener('click', () => { 
+    localStorage.setItem("envio", "si")
+    totalCompra = Number(totalCompra) + 600//SI AGREGA EL ENVIO, SUMAMOS EL ENVIO AL CARRITO DEL NAV
+    localStorage.setItem("totalCompra",totalCompra)
+    pintarCarrito()
+})
+
+
 
 
 const pintarCarrito = () => {
@@ -49,8 +72,8 @@ Object.values(carrito).forEach(prod =>{
 })
 items.appendChild(fragment)
 pintarFooter()
-    localStorage.setItem("carrito", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
-    document.getElementById('totalCompraNav').innerHTML=`$ ${totalCompra}`
+localStorage.setItem("carrito", JSON.stringify(carrito));//USUARIO DEBERÍA USAR EL ID DEL USUARIO
+document.getElementById('totalCompraNav').innerHTML=`$ ${totalCompra}`
 }
 
 const pintarFooter = () => {
@@ -75,21 +98,21 @@ const pintarFooter = () => {
         localStorage.setItem("totalCompra", totalCompra)
         document.getElementById('totalCompraNav').innerHTML = `$ ${totalCompra}`
         })
-        pintarEnvio()
+        pintarEnvio()       
 }
 const btnAccion = e => { 
     if (e.target.classList.contains('btn-info')) { 
         const producto = carrito[e.target.dataset.id]
         producto.cantidad ++
         carrito[e.target.dataset.id] = { ...producto }
-        totalCompra = totalCompra + producto.precio
+        totalCompra = Number(totalCompra) + Number(producto.precio)
         localStorage.setItem("totalCompra",totalCompra)
         pintarCarrito()
     }
     if (e.target.classList.contains('btn-danger')) { 
         const producto = carrito[e.target.dataset.id]
         producto.cantidad--
-        totalCompra = totalCompra - producto.precio
+        totalCompra = Number(totalCompra) - Number(producto.precio)
         localStorage.setItem("totalCompra", totalCompra)
         if (producto.cantidad === 0) { 
             delete carrito[e.target.dataset.id]
@@ -102,14 +125,25 @@ const btnAccion = e => {
 
 //PINTA LA TABLA DEL ENVIO
 const pintarEnvio = () => { 
-    envios.innerHTML=''
-    templateEnvio.getElementById('nombre').textContent = user.nombre
-    templateEnvio.getElementById('apellido').textContent = user.apellido
-    templateEnvio.getElementById('direccion').textContent = user.direccion
-    templateEnvio.getElementById('telefono').textContent = user.telefono
-    templateEnvio.getElementById('costo').textContent = 600
-    templateEnvio.getElementById('total-costo').textContent = Number(document.getElementById('total-compra').textContent) + 600
-    const clone = templateEnvio.cloneNode(true)
-    fragment.appendChild(clone)
-    envios.appendChild(fragment)
+    envio = localStorage.getItem("envio")
+    if (envio === "si") {    
+        rbEnvioLocal.checked=false
+        rbEnvioDomicilio.checked=true
+        envios.innerHTML = ''
+        templateEnvio.getElementById('nombre').textContent = user.nombre
+        templateEnvio.getElementById('apellido').textContent = user.apellido
+        templateEnvio.getElementById('direccion').textContent = user.direccion
+        templateEnvio.getElementById('telefono').textContent = user.telefono
+        templateEnvio.getElementById('costo').textContent = 600
+        templateEnvio.getElementById('total-costo').textContent = Number(document.getElementById('total-compra').textContent) + 600
+        const clone = templateEnvio.cloneNode(true)
+        fragment.appendChild(clone)
+        envios.appendChild(fragment)
+    } else { 
+        rbEnvioLocal.checked=true
+        rbEnvioDomicilio.checked=false
+        envios.innerHTML = ''
+        envios.appendChild(fragment)
+    }
+    
 }
